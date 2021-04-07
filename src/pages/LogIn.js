0e69590e88ee4e78a0667047cc11f3ac5d483d6b
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-07 11:33:13 -0700

    added login form and directed to Home page

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Home() {
+export default function LogIn() {
     return (
         <div>
-            <h1>LogIn</h1>
-        </div>
+            <h2>LogIn</h2>
+            {/* <form onSubmit={this.handleSubmit}> */}
+            <form>
+                <div className="form-input">
+                    <label htmlFor="title">Username</label>
+                    <input 
+                    type="text" 
+                    name="title" /> 
+                    {/* // onChange={this.handleChange}
+                    // value={this.state.title} /> */}
+                </div>
+                <div className="form-input">
+                    <label htmlFor="publisher">Password</label>
+                    <input 
+                    type="text" 
+                    name="publisher" />
+                    {/* // onChange={this.handleChange}
+                    // value={this.state.publisher} /> */}
+                </div>
+            </form>
+            <div>
+            <Link to="/Home">
+                <button className="loginpage-btn">LOGIN</button>
+            </Link>
+            </div>
+      </div>
     )
 }
