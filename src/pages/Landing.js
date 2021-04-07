bc7840a08b49190ba6ed747664d47025cb9cb98f
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-07 11:33:51 -0700

    added layout with buttons

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom'
 export default function Landing() {
     return (
         <>
-        <div className="this">
-            <p>THIS</p>
-        </div>
-        <div className="that">
-            <p>THAT</p>
-        </div>
-        <div className="that">
-            <p>?</p>
-        </div>
-        <div>
-            <Link to="/LogIn">
-                <button className="login-btn">LOGIN</button>
-            </Link>
-            <Link to="/SignUp">
-                <button className="signup-btn">SIGN UP</button>
-            </Link>
-        </div>
+            <div className="this">
+                <p>THIS</p>
+            </div>
+            <div className="that">
+                <p>THAT</p>
+            </div>
+            <div className="that">
+                <p>?</p>
+            </div>
+            <div>
+                <Link to="/LogIn">
+                    <button className="login-btn">LOGIN</button>
+                </Link>
+                <Link to="/SignUp">
+                    <button className="signup-btn">SIGN UP</button>
+                </Link>
+            </div>
         </>
     )
 }
