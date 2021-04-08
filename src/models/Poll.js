7db32e2bcedb8121a0b10fb6fa4a1d54ef2e8427
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-08 01:08:24 -0700

    added crud to poll model

--- src/models/Poll.js
+++ src/models/Poll.js
@@ -1,29 +1,26 @@
-
-
-
+const url = `http://localhost:4000/api/v1`
 
 
 class PollModel {
-    // the "static" keyword allows us to invokd the method without an instance of the class
-    static all = () => {
-      return fetch(`https://localhost/4000/polls`).then(res => res.json())
-    }
-  
-    static show = (pollId) => {
-      return fetch(`https://localhost/4000/polls/${pollId}`).then(res => res.json())
-    }
-  
-    static create = (pollData) => {
-      return fetch(`https://localhost/4000/polls`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(pollData)
-      })
-        .then(res => res.json())
-    }
+  // the "static" keyword allows us to invoke the method without an instance of the class
+  static all = () => {
+    return fetch(`${url}/polls`).then(res => res.json())
   }
-  
-  export default PollModel
-  
+
+  static show = (pollId) => {
+    return fetch(`${url}/polls/${pollId}`).then(res => res.json())
+  }
+
+  static create = (pollData) => {
+    return fetch(`${url}/polls`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(pollData)
+    })
+      .then(res => res.json())
+  }
+}
+
+export default PollModel
