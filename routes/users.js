d814f1d887507454e697b79d861eaf338cdeef4c
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-08 01:22:28 -0700

    corrected routes

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const ctrl = require('../controllers');
 
 
-
 router.get('/', ctrl.users.index);
 router.get('/:id', ctrl.users.show);
 router.post('/', ctrl.users.create);
