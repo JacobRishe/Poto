d682e7a51944620706dc83eaf39b7f53bea6dd16
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-08 01:20:37 -0700

    added crud

--- controllers/users.js
+++ controllers/users.js
@@ -1,43 +1,52 @@
 const db = require("../models");
-const mongoose = require('mongoose')
 
 
 
 const index = (req, res) => {
     db.User.find({}, (err, foundusers) => {
         if (err) console.log('Error in users#index:', err)
-        if(!foundUsers) return res.json({
-            message: 'No Users found in database.'
-        })
+        if(!foundUsers) return res.json({ message: 'No Users found in database.' })
         console.log(foundUsers)
-        res.status(200).json({ Users: foundUsers });
+        res.json({ Users: foundUsers });
     })
 }
+
+
 const show = (req, res) => {
     db.User.findById(req.params.id, (err, foundUser) => {
         if (err) console.log('Error in Users#show:', err);
-        res.send("Incomplete Users#show controller function");
+        
+        if (!foundGame) return res.json({ message: "Game with that ID was not found." })
+
+        res.json({ game: foundGame })
     });
 };
+
+
 const create = (req, res) => {
     db.User.create(req.body, (err, savedUser) => {
         if (err) console.log('Error in users#create:', err)
-        // Validations and error handling here
-        res.status(201).json({ user: savedUser })
+        res.json({ user: savedUser })
     })
 }
+
+
 const update = (req, res) => {
     db.User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedUser) => {
         if (err) console.log('Error in Users#update:', err);
-        res.send("Incomplete users#update controller function");
+        
+        res.json({ user: updatedUser });
     });
 };
+
+
 const destroy = (req, res) => {
     db.User.findByIdAndDelete(req.params.id, (err, deletedUser) => {
         if (err) console.log('Error in Users#destroy:', err);
-        res.send("Incomplete users#destroy controller function");
+        res.json({ message: "User removed." });
     });
 };
+
 module.exports = {
     index,
     show,
