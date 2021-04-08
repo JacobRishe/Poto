a12fad1aec9dc8b97d6b1dcd4d730e93832ecb98
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-08 01:11:05 -0700

    added crud to user model

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+const url = `http://localhost:4000/user`
+
+
+class UserModel {
+  // the "static" keyword allows us to invoke the method without an instance of the class
+  static all = () => {
+    return fetch(`${url}/users`).then(res => res.json())
+  }
+
+  static show = (userId) => {
+    return fetch(`${url}/users/${userId}`).then(res => res.json())
+  }
+
+  static create = (userData) => {
+    return fetch(`${url}/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userData)
+    })
+      .then(res => res.json())
+  }
+}
+
+export default UserModel
