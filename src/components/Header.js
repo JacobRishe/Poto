575bda47938795e929a06b3d1b90322fa5c28c5c
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-08 01:03:28 -0700

    removed paragraph tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <div className="poll-together">
-         <p>Poll Together</p>
+            {/* <p>Poll Together</p> */}
         </div>
     )
 }
