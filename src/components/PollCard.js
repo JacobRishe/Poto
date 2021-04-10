73fbbfb990dfc1e6bd103996409e70e547e822df
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-10 12:16:46 -0700

    added yes count and no count

--- src/components/PollCard.js
+++ src/components/PollCard.js
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
-// import './PollCard.scss'
+import PollModel from '../models/Poll';
+
 
 const PollCard = (props) => {
 
   const [count, setCount] = useState(0)
 
   function noCount() {
-    setCount(prevCount => prevCount - 1)
+    PollModel.no().then(data => {
+      console.log(data)
+  })
 }
 
-function yesCount() {
-    setCount(prevCount => prevCount + 1)
+  function yesCount() {
+    PollModel.yes().then(data => {
+      console.log(data)
+  })
 }
 
   return (
     <div className="pollcard">
       <h3>{ props.question }</h3>
-      <p>By: { props.author }</p>
+      <p>{ props.author }</p>
       <button onClick={ yesCount }>Yes</button>
       <span>{ count }</span>
       <button onClick={ noCount }>No</button>
