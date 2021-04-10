6175ac74e6e83564c9fa2fd538e76c955c455fba
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-09 23:21:41 -0700

    added footer class to text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className="footer">
-            <p>Designed for indecisive people</p>
+            <p className='footer-text'>Designed for indecisive people</p>
         </div>
     )
 }
