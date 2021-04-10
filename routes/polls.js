52e2df2a3aadd427e063381d3c48f5fefb599d41
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-10 12:18:51 -0700

    added routes for voteyes and voteno

--- routes/polls.js
+++ routes/polls.js
@@ -8,6 +8,8 @@ router.post('/', ctrl.polls.create);
 router.put('/:id', ctrl.polls.update);
 router.delete('/:id', ctrl.polls.destroy);
 router.post('/:id/vote', ctrl.polls.vote);
+router.post('/:id/voteyes', ctrl.polls.voteyes);
+router.post('/:id/voteno', ctrl.polls.voteno);
 
 
 
