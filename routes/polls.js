392e8917f8ffbec61fbf21e8deaffe8496f7490c
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-09 20:19:37 -0700

    added route to vote

--- routes/polls.js
+++ routes/polls.js
@@ -7,6 +7,7 @@ router.get('/:id', ctrl.polls.show);
 router.post('/', ctrl.polls.create);
 router.put('/:id', ctrl.polls.update);
 router.delete('/:id', ctrl.polls.destroy);
+router.post('/:id/vote', ctrl.polls.vote);
 
 
 
