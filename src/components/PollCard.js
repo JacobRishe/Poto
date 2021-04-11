734d093f8b4ab3e8ed50f71b24011f844b5ee89c
JacobRishe <Jacob.Rishe@gmail.com>
2021-04-10 21:31:05 -0700

    deleted counter

--- src/components/PollCard.js
+++ src/components/PollCard.js
@@ -1,20 +1,39 @@
+
 import React, { useState } from 'react';
 import PollModel from '../models/Poll';
 
 
 const PollCard = (props) => {
 
+  const [percent, setPercent] = useState(0)
+  const [yes, setYes] = useState(0)
   const [count, setCount] = useState(0)
+  const [clicked, setClicked] = useState(false)
 
   function noCount() {
-    PollModel.no().then(data => {
+    console.log(props.question)
+    PollModel.no(props.questionid).then(data => {
       console.log(data)
+      setClicked(true)
+      let temp = data.no
+      temp += data.yes
+      setCount(temp)
+      setYes(data.yes)
+      let answer = ((data.yes / temp) * 100).toFixed(0)
+      setPercent(String(answer) + "%")
   })
 }
 
   function yesCount() {
-    PollModel.yes().then(data => {
+    PollModel.yes(props.questionid).then(data => {
       console.log(data)
+      setClicked(true)
+      let temp = data.yes
+      temp += data.no
+      setCount(temp)
+      setYes(data.yes)
+      let answer = ((data.yes / temp) * 100).toFixed(0)
+      setPercent(String(answer) + "%")
   })
 }
 
@@ -22,10 +41,13 @@ const PollCard = (props) => {
     <div className="pollcard">
       <h3>{ props.question }</h3>
       <p>{ props.author }</p>
-      <button onClick={ yesCount }>Yes</button>
-      <span>{ count }</span>
-      <button onClick={ noCount }>No</button>
-      <hr />
+      {!clicked ?  <>
+        <button className="yes-btn" onClick={ yesCount }>Yes</button>
+        <button className="no-btn"onClick={ noCount }>No</button>
+        <hr />
+        </>: 
+        <p>{ percent }</p>
+      }
     </div>
   );
 }
